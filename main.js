35128455d8a0f449eb1fa5c0af3fa809f2507a80
Veronica isla <veroislagleaniz@gmail.com>
2022-11-02 16:21:36 +0100

    created simple button using javascript

--- main.js
+++ main.js
@@ -2,3 +2,7 @@
 
 // Apartado 1a
 // Escribe el código javascript necesario para pintar un botón en el body y que el botón tenga el texto "1 botón"
+
+const button = document.querySelector('.js__button');
+
+button.innerHTML += '<button type="button">1 botón</button>';
