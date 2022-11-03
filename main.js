9fa11863a7978403b90157a593e2d530a2e80fdb
Veronica isla <veroislagleaniz@gmail.com>
2022-11-03 10:47:08 +0100

    change text buttons on mouseover and mouseout

--- main.js
+++ main.js
@@ -3,5 +3,13 @@
 for (let i = 1; i < 1001; i++) {
   const button = document.createElement('button');
   button.innerHTML += `${i} Botón`;
+
+  button.addEventListener('mouseover', () => {
+    button.innerHTML = `${i} BotóN`;
+  });
+  button.addEventListener('mouseout', () => {
+    button.innerHTML = `${i} Botón`;
+  });
+
   document.body.appendChild(button);
 }
