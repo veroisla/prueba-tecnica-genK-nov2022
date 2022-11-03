a945301f4a0a8f149020aca1433f7abc95dec306
Veronica isla <veroislagleaniz@gmail.com>
2022-11-03 12:34:47 +0100

    added solution from exercise 2A

---
+++ main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const array1 = [
+  [1, 3, 5],
+  [7, 9, 7],
+  [5, 3, 1],
+];
+
+const array2 = [
+  [2, 4, 6],
+  [8, 10, 8],
+  [6, 4, 2],
+];
+
+let result = [];
+
+for (let rows = 0; rows < 3; rows++) {
+  let newArray = [];
+
+  for (let columns = 0; columns < 3; columns++) {
+    newArray.push(array1[rows][columns] * array2[columns][rows]);
+  }
+  result.push(newArray);
+}
+
+console.log(result);
