b73506b9a08c805264ccd073dddbc87891388578
Veronica isla <veroislagleaniz@gmail.com>
2022-11-03 09:48:05 +0100

    created function to change the tex of the button on mouseover

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const button = document.querySelector('.js__button');
+
+button.innerHTML += '<button type="button" class="js__btn">1 botón</button>';
+
+function changeTextHover() {
+  button.innerHTML = '<button type="button">1 botóN</button>';
+}
+
+button.addEventListener('mouseover', changeTextHover);
